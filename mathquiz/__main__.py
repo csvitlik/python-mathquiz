3a1cfcc781574f15be123d59203c3ca3d0dd6bd3
Charlie Svitlik <csvitlik@timex.com>
2017-03-11 21:47:12 -0500

    Split number pair generation out.

--- mathquiz/__main__.py
+++ mathquiz/__main__.py
@@ -5,13 +5,15 @@ import sys
 if sys.version[0] == '3':
     raw_input = input
 
-def myeval(a,op,b):
+
+def myeval(a, op, b):
     result = None
     try:
-        result = eval('{}{}{}'.format(a,op,b))
+        result = eval('{}{}{}'.format(a, op, b))
     except:
         pass
     return result
+
 
 def myinput(s):
     result = None
@@ -20,6 +22,7 @@ def myinput(s):
     except:
         pass
     return result
+
 
 def numberatlevel(level):
     levels = [6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24, 28, 30, 32]
@@ -31,38 +34,43 @@ def numberatlevel(level):
 
     return random.randint(2, 2**levels[level])
 
+
+def randompair(level):
+    a = numberatlevel(level)
+    b = numberatlevel(level)
+    tries = 0
+    while a == b or 0 != (a % b):
+        tries += 1
+        b = numberatlevel(level)
+        if tries > 9999:
+            a = numberatlevel(level)
+            tries = 0
+
+    return a, b
+
+
 def main():
-    level = 3 # current level
-    ops = ['+', '-', '*', '/', '**'] # supported operations
-    op = ops[0] # current operation
-    running = True # currently running
+    level = 3  # current level
+    ops = ['+', '-', '*', '/', '**']  # supported operations
+    op = ops[0]  # current operation
+    running = True  # currently running
 
     while running:
-        a = numberatlevel(level)
-        b = numberatlevel(level)
-        tries = 0
-        while a == b or 0 != (a % b):
-            tries += 1
-            b = numberatlevel(level)
-            if tries > 9999:
-                a = numberatlevel(level)
-                tries = 0
+        a, b = randompair(level)
+        ans = myeval(a, op, b)
 
-
-        ans = myeval(a,op,b)
         while True:
-            print(level)
-            res = myinput('  {:10d}\n{} {:10d}\n'.format(a,op,b))
+            res = myinput('  {:10d}\n{} {:10d}\n'.format(a, op, b))
 
             if res == None or res == 'q':
                 running = False
                 break
 
             if res == '<':
-                level -= 1;
+                level -= 1
 
             if res == '>':
-                level += 1;
+                level += 1
 
             if res == '?':
                 print(int(ans))
